75299d015f14efdda1b4f704d88ece3a90876950
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2023-02-01 13:24:59 -0300

    Creacion de Simulador de venta de entradas

---
+++ main.js
@@ -0,0 +1,93 @@
+function logIn() {
+    user = prompt('Ingresá tu nombre de usuario');
+    pass = prompt('Ingresa tu contraseña');
+}
+
+function pagar() {
+    if (sector == 'Campo General') {
+        let campoGeneral = 5000 * cantidad;
+        alert('El costo total de tus entradas es de $' + campoGeneral)
+    } else if (sector == 'Campo VIP') {
+        let campoVip = 10000 * cantidad;
+        alert('El costo total de tus entradas es de $' + campoVip)
+    }
+
+
+}
+
+let user = 0;
+let pass = 0;
+let band = 0;
+let ticket = 'ok'
+let sector = 0;
+let cantidad = 0;
+let band1 = 'Imagine Dragons'
+let band2 = 'Polo & Pan'
+let band3 = 'The Weeknd'
+let band4 = 'Cigarrets After Sex'
+let band5 = 'Muse'
+let band6 = 'Nicolas Jaar'
+let band7 = 'Lana del Rey'
+let band8 = 'Beach House'
+let band9 = 'Nine Inch Nails'
+let band10 = 'La Femme'
+
+// Inicio del Programa 
+
+logIn();
+
+while (ticket !== '1') {
+
+    band = parseInt(prompt('Por favor ' + user + ', ingresá el número del evento al que queres ir: \n\n 1 - ' + band1 + ' \n 2 - ' + band2 + ' \n 3 - ' + band3 + '\n 4 - ' + band4 + ' \n 5 - ' + band5 + ' \n 6 - ' + band6 + ' \n 7 - ' + band7 + ' \n 8 - ' + band8 + ' \n 9 - ' + band9 + ' \n 10 - ' + band10))
+
+    switch (band) {
+        case 1:
+            band = band1;
+            break;
+        case 2:
+            band = band2;
+            break;
+        case 3:
+            band = band3;
+            break;
+        case 4:
+            band = band4;
+            break;
+        case 5:
+            band = band5;
+            break;
+        case 6:
+            band = band6;
+            break;
+        case 7:
+            band = band7;
+            break;
+        case 8:
+            band = band8;
+            break;
+        case 9:
+            band = band9;
+        case 10:
+            band = band10;
+            break;
+        default:
+            band = 'Opción incorrecta'
+            break;
+    }
+
+    sector = prompt('Ingresa a que sector queres ir: \n\n 1 - Campo General \n 2 - Campo VIP');
+    if (sector == 1) {
+        sector = 'Campo General'
+    } else if (sector == 2) {
+        sector = 'Campo VIP'
+    } else {
+        sector = 'Opción incorrecta'
+    }
+
+    cantidad = Number(prompt('Cuantas entradas para ' + sector + ' queres?'));
+    alert('Elegiste ' + cantidad + ' entradas ' + sector + ' para ir a ver a ' + band)
+    ticket = prompt('Ingresa 1 para continuar o presiona cualquier tecla para hacer alguna modificación');
+}
+
+
+pagar();
