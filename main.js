bdf298f02d8400b862466df6e687bd03e829359d
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2023-02-16 17:41:47 -0300

    Se implemento el principio de registro interno en consola simulando database

--- main.js
+++ main.js
@@ -31,31 +31,22 @@ class Show {
         this.precio = precio;
         this.payment = payment;
         this.info = ` Banda: ${band} \n Sector: ${sector} \n Cantidad de entradas: ${cantidad} \n Precio: $${precio} \n ${payment} `;
-
     }
-
 }
 
 function logIn() {
-
     user = prompt('Ingresá tu nombre de usuario:').toLocaleLowerCase();
     pass = parseInt(prompt('Ingresá tu contraseña'));
-
     while (user !== 'emiliano' || pass !== 123) {
         alert('Usuario no registrado, vuelva a intentarlo.');
         logIn();
     }
 }
 
-
-
 function buy() {
-
     do {
         band = parseInt(prompt('Por favor ' + user + ', ingresá el número del evento al que queres ir: \n\n 1 - ' + band1 + ' \n 2 - ' + band2 + ' \n 3 - ' + band3 + '\n 4 - ' + band4 + ' \n 5 - ' + band5 + ' \n 6 - ' + band6 + ' \n 7 - ' + band7 + ' \n 8 - ' + band8 + ' \n 9 - ' + band9 + ' \n 10 - ' + band10));
-
     } while (band < 1 || band > 10 || (isNaN(band)));
-
 
     switch (band) {
         case 1:
@@ -91,10 +82,8 @@ function buy() {
     }
 
     do {
-        sector = prompt('Ingresa a que sector queres ir: \n\n 1 - Campo General \n 2 - Campo VIP');
-
+        sector = prompt('Ingresá a que sector queres ir: \n\n 1 - Campo General \n 2 - Campo VIP');
     } while (sector != 1 && sector != 2 || (isNaN(sector)));
-
     if (sector == 1) {
         sector = 'Campo General';
     } else if (sector == 2) {
@@ -102,7 +91,7 @@ function buy() {
     }
 
     do {
-        cantidad = parseInt(prompt('Cuantas entradas para ' + sector + ' queres?'))
+        cantidad = parseInt(prompt('Cuantas entradas para ' + sector + ' queres?'));
     } while (isNaN(cantidad));
 
     if (sector == 'Campo General') {
@@ -111,115 +100,78 @@ function buy() {
         precio = 10000 * cantidad;
     }
 
-
-
-
-
     do {
-        payment = parseInt(prompt('Elegi el numero de la opcion deseada: \n 1. Pago en efectivo \n 2. Pago en 3 cuotas sin interes con tarjeta Visa o Mastercard'))
-    } while (isNaN(payment) || payment != 1 && payment != 2)
+        payment = parseInt(prompt('Elegí el número de la opción deseada: \n 1. Pago en efectivo \n 2. Pago en 3 cuotas sin interés con tarjeta Visa o Mastercard'));
+    } while (isNaN(payment) || payment != 1 && payment != 2);
 
     switch (payment) {
         case 1:
             payment = 'Efectivo';
-            break
+            break;
         case 2:
             payment = 'Tarjeta';
             break;
     }
 
-
-
-
-
-
-
-
     const evento = new Show(band, sector, cantidad, precio, payment);
-
-
     alert(evento.info);
-
-
     arrayShop.push(evento);
 }
 
 function gift() {
-
     parking = arrayShop.find(evento => evento.sector == 'Campo VIP');
-
     if (parking) {
-        alert(' BENEFICIO EXCLUSIVO \n Por la compra de entradas VIP tenes estacionamiento gratuito para todos los eventos.')
+        alert('BENEFICIO EXCLUSIVO \nPor la compra de entradas VIP, Te regalamos el acceso exclusivo al Estacionamiento y aplica para todos los eventos.');
         console.log('Estacionamiento gratuito: Si');
-
     } else {
         console.log('Estacionamiento gratuito: No');
-
     }
 }
-
-
 
 
 // Inicio del Programa 
 
 logIn();
 
-
 while (menu != 3) {
-    menu = prompt('Elegi el número de la opción deseada: \n \n 1. Comprar tickets \n 2. Ver Carrito \n 3. Finalizar compra \n');
-
+    menu = prompt('Elegí el número de la opción deseada: \n \n 1. Comprar tickets \n 2. Ver Carrito \n 3. Finalizar compra \n');
     if (menu == 1) {
         buy();
-
-
-
-
-
     } else if (menu == 2) {
-
         arrayShop.find(evento => {
             alert(evento.info);
-            console.log(arrayShop)
         })
 
-
-
-
-
-
-
+        console.log('    ShoWbiz  ===============================================');
+        console.log(' DETALLE DE TICKETS Y EVENTOS CARGADOS\n   (Disponible explorando Array)  ');
+        console.log(arrayShop);
 
     } else if (menu == 3) {
-
-
         arrayShop.forEach((evento) => {
             subTotal.push(evento.precio)
         });
         for (let i = 0; i < subTotal.length; i++) {
-            precioFinal = precioFinal + subTotal[i]
+            precioFinal = precioFinal + subTotal[i];
         };
 
-        arrayCard = arrayShop.filter((evento) => evento.payment == 'Tarjeta')
+        arrayCard = arrayShop.filter((evento) => evento.payment == 'Tarjeta');
+        arrayCash = arrayShop.filter((evento) => evento.payment == 'Efectivo');
 
-        arrayCash = arrayShop.filter((evento) => evento.payment == 'Efectivo')
+        alert('PRECIO FINAL A PAGAR: $' + precioFinal);
+        console.log('Monto total abonado: ' + precioFinal);
 
-
-
-        console.log(arrayShop)
-        console.log('El costo de total de tus entradas es de: $ ' + precioFinal);
-        console.log('Forma de pago: ' + payment)
-        alert('El costo de total de las entradas es de $' + precioFinal);
         gift();
 
-        console.log('Eventos cobrados en efectivo: ')
-        console.log(arrayCash)
-        console.log('Eventos cobrados en tarjeta: ')
-        console.log(arrayCard)
+        console.log('===============================================');
 
+        console.log('Eventos cobrados en efectivo: \n Ver detalles en Array');
+        console.log(arrayCash);
+        console.log('Eventos cobrados en tarjeta: \n Ver detalles en Array ');
+        console.log(arrayCard);
 
+        console.log('===============================================');
 
-        alert('Gracias por su visita!');
+        alert('MUCHAS GRACIAS POR SU COMPRA, HASTA PRONTO !');
     }
 }
 
